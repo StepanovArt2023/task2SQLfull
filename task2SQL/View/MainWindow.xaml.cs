42cbd8692bf0825216c284dea03a10b5c1011702
User <User@dkit.com>
2023-04-18 10:06:28 +0300

    Добавьте файлы проекта.

---
+++ task2SQL/View/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using task2SQL.Model;
+using task2SQL.View;
+
+namespace task2SQL
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SASEntities _db = new SASEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private async void BtnLogin_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Role userModel = await _db.Roles.FirstOrDefaultAsync(d => d.Role1 == TbLogin.Text);
+                if (userModel != null)
+                {
+                    switch (userModel.RoleID)
+                    {
+                        case 1:
+                            new AdminWindow().ShowDialog();
+                            break;
+                        case 2:
+                            new DevWindow().ShowDialog();
+                            break;
+                        case 3:
+                            new UserWindow().ShowDialog();
+                            break;
+                    }
+                }
+                else
+                {
+                    new ErrorWindow().ShowDialog();
+                }
+            }
+            catch (Exception)
+            {
+
+                new ErrorWindow().ShowDialog();
+            }
+        }
+
+        private void BtnAdminInfo_Click(object sender, RoutedEventArgs e)
+        {
+            TbLogin.Text = "admin";
+            PbPassword.Password = "11111";
+        }
+
+        private void BtnDevInfo_Click(object sender, RoutedEventArgs e)
+        {
+            TbLogin.Text = "dev";
+            PbPassword.Password = "22222";
+        }
+
+        private void BtnUserInfo_Click(object sender, RoutedEventArgs e)
+        {
+            TbLogin.Text = "user";
+            PbPassword.Password = "33333";
+        }
+
+        private void BtnClearInfo_Click(object sender, RoutedEventArgs e)
+        {
+            TbLogin.Text = string.Empty;
+            PbPassword.Password = string.Empty;
+        }
+    }
+}
